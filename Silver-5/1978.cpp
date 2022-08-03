48c635c5f316a1394dd4e161a93a84a6d63020e7
Nabin Kim <knabinn97@gmail.com>
2022-08-03 21:57:46 +0900

    1978. 소수 찾기

---
+++ Silver-5/1978.cpp
@@ -0,0 +1,51 @@
+/*
+ * 제목     소수 찾기
+ * 결과     맞았습니다!!
+ * 메모리   2024 KB
+ * 시간     0 ms
+ * 언어     C++17
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, max = INT_MIN, res = 0;
+    vector<int> vInput;
+    vector<bool> vRes;
+
+    cin >> n;
+    vInput.resize(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> vInput[i];
+        if (vInput[i] > max) max = vInput[i];
+    }
+
+    vRes.resize(max);
+
+    for (int i = 2; i <= max; ++i)
+    {
+        if (vRes[i] == true) continue;
+        for (int j = i * 2; j <= max; j += i)
+        {
+            vRes[j] = true;
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (vInput[i] != 1 && vRes[vInput[i]] == false)
+        {
+            ++res;
+        }
+    }
+
+    cout << res;
+    
+    return 0;
+}
