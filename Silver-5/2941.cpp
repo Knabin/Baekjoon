2eb12355646f662424b36f5cdc503acc774247f1
Nabin Kim <knabinn97@gmail.com>
2022-08-05 22:26:51 +0900

    2941. 크로아티아 알파벳

---
+++ Silver-5/2941.cpp
@@ -0,0 +1,60 @@
+/*
+ * 제목     크로아티아 알파벳
+ * 결과     맞았습니다!!
+ * 메모리   2024 KB
+ * 시간     0 ms
+ * 언어     C++17
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> vStr{ "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+int res = 0;
+
+int func(const string& str)
+{
+    for (int i = 0; i < vStr.size(); ++i)
+    {
+        int index = str.find(vStr[i]);
+        if (index == 0) 
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str, temp;
+    cin >> str;
+
+    int index = 0;
+
+    while(true)
+    {
+        if (str.length() <= index) break;
+
+        temp = str.substr(index);
+        auto te = func(temp);
+
+        if (te == -1)
+        {
+            ++index;
+        }
+        else
+        {
+            index += vStr[te].size();
+        }
+
+        ++res;
+    }
+
+    cout << res;
+
+    return 0;
+}
