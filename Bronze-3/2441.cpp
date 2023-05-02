ad86265539cbbaeb3e2f17189a29a2a2bd08a0b4
Nabin Kim <knabinn97@gmail.com>
2023-05-02 21:04:46 +0900

    2441. 별 찍기 - 4

---
+++ Bronze-3/2441.cpp
@@ -0,0 +1,30 @@
+/*
+ * 제목     별 찍기 - 4
+ * 결과     맞았습니다!!
+ * 메모리   2020 KB
+ * 시간     0 ms
+ * 언어     C++17
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            cout << ' ';
+        }
+        for (int k = 0; k < n - i; ++k) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
